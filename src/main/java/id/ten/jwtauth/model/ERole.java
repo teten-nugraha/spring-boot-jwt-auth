772b2dbb83d8e503d44d1178c4e44446725fe153
Teten Nugraha <teten.nugraha@allianz.co.id>
2021-06-03 14:14:38 +0700

    setup domain model

---
+++ src/main/java/id/ten/jwtauth/model/ERole.java
@@ -0,0 +1,7 @@
+package id.ten.jwtauth.model;
+
+public enum ERole {
+    ROLE_USER,
+    ROLE_MODEDATOR,
+    ROLE_ADMIN
+}
---
+++ src/main/java/id/ten/jwtauth/model/Role.java
@@ -0,0 +1,32 @@
+package id.ten.jwtauth.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "roles")
+@Data
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private ERole name;
+}
