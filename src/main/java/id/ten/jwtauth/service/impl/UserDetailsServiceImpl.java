0522461a4932465be778509912df92f3a6a5530d
Teten Nugraha <teten.nugraha@allianz.co.id>
2021-06-04 09:39:28 +0700

    UserDetailsService

--- src/main/java/id/ten/jwtauth/service/impl/UserDetailsServiceImpl.java
+++ src/main/java/id/ten/jwtauth/service/impl/UserDetailsServiceImpl.java
@@ -2,13 +2,13 @@ package id.ten.jwtauth.service.impl;
 
 import id.ten.jwtauth.model.User;
 import id.ten.jwtauth.repository.UserRepository;
-import id.ten.jwtauth.service.UserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
-import javax.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Class ini digunakan untuk mendapatkan username dari suatu database
@@ -17,13 +17,21 @@ import javax.transaction.Transactional;
  * @author TetenNugraha
  * @since 03-06-2021
  */
+@Qualifier("user-detail-service")
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Autowired
     private UserRepository userRepository;
 
-
+    /**
+     * load user object by username
+     * connect with repository
+     *
+     * @param username
+     * @return
+     * @throws UsernameNotFoundException
+     */
     @Override
     @Transactional
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
